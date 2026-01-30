5d278680ce436f98105515b12aecf12e4e91c70a
FireFly-Dev <1223010034@qq.com>
2026-01-30 13:34:44 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using MelonLoader;
+
+namespace HookQuit
+{
+	public class Main : MelonMod { }
+
+	public class Patcher 
+	{
+		[HarmonyPatch(typeof(UnityEngine.Application), "Quit", new System.Type[]
+	{
+
+	})]
+		public class Quit_Hook
+		{
+			[HarmonyPrefix]
+			internal static bool Prefix()
+			{
+				return false;
+			}
+		}
+	}
+}
